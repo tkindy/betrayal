8c85f53285e4a5716acf77a48fca7324dff4c0d1
Tyler Kindy <tylerkindy@gmail.com>
2021-04-24 16:23:47 -0400

    Extract function to connect to database

--- api/src/main/kotlin/com/tylerkindy/betrayal/Main.kt
+++ api/src/main/kotlin/com/tylerkindy/betrayal/Main.kt
@@ -1,31 +1,22 @@
 package com.tylerkindy.betrayal
 
 import com.tylerkindy.betrayal.routes.gameRoutes
-import com.tylerkindy.betrayal.routes.lobbyRoutes
-import com.zaxxer.hikari.HikariDataSource
+import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.callloging.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.websocket.*
+import io.ktor.websocket.*
 import kotlinx.serialization.json.Json
-import org.jetbrains.exposed.sql.Database
 import org.slf4j.event.Level
 import java.time.Duration
 
 fun main() {
-    Database.connect(
-        HikariDataSource().apply {
-            jdbcUrl = System.getenv("JDBC_DATABASE_URL")
-        }
-    )
+    connectToDatabase()
     val port = System.getenv("PORT")?.toInt() ?: 8080
 
     embeddedServer(Netty, port = port) {
@@ -39,13 +30,13 @@ fun main() {
         }
         install(CORS) {
             allowNonSimpleContentTypes = true
-            allowHost("localhost:3000")
-            allowHost(
+            host("localhost:3000")
+            host(
                 "herokuapp.com",
                 schemes = listOf("https"),
                 subDomains = listOf("betrayal-ui")
             )
-            allowHost(
+            host(
                 "tylerkindy.com",
                 schemes = listOf("http", "https"),
                 subDomains = listOf("betrayal")
@@ -58,17 +49,16 @@ fun main() {
             pingPeriod = Duration.ofSeconds(30)
         }
         install(StatusPages) {
-            exception<IllegalArgumentException> { call, cause ->
+            exception<IllegalArgumentException> {
                 call.respond(
                     HttpStatusCode.BadRequest,
-                    mapOf("message" to cause.message)
+                    mapOf("message" to it.message)
                 )
-                throw cause
+                throw it
             }
         }
 
         routing {
-            lobbyRoutes()
             gameRoutes()
         }
     }.start(wait = true)
