2a53ce88ba5fff77ad9811df3f5916a795f4d250
Tyler Kindy <tylerkindy@gmail.com>
2021-04-29 09:04:38 -0400

    More work on the job

--- api/src/main/kotlin/com/tylerkindy/betrayal/db/Tables.kt
+++ api/src/main/kotlin/com/tylerkindy/betrayal/db/Tables.kt
@@ -31,8 +31,10 @@ object Rooms : Table() {
     val gridX = integer("gridX")
     val gridY = integer("gridY")
     val rotation = short("rotation")
+    val floor = varchar("floor", 1).nullable()
 
-    override val primaryKey = PrimaryKey(id)
+    override
+    val primaryKey = PrimaryKey(id)
 }
 
 object RoomStacks : Table("\"roomStacks\"") {
