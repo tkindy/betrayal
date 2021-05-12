f2ff1f085764c6a2355c53b0c31ae874ed38484d
Tyler Kindy <tylerkindy@gmail.com>
2021-05-11 21:14:25 -0400

    Track expanded

--- ui/src/components/game/sidebar/FindInStacks.tsx
+++ ui/src/components/game/sidebar/FindInStacks.tsx
@@ -1,12 +1,26 @@
-import React, { FC } from 'react';
+import { FC, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { searchStacks } from '../../../features/search';
+import { useAppDispatch } from '../../../hooks';
+import { RootState } from '../../../store';
 
 const FindInStacks: FC<{}> = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <input
       style={{
         textAlign: 'center',
       }}
       placeholder="Find in stacks"
+      ref={inputRef}
+      onFocus={() => {
+        setExpanded(true);
+      }}
+      onBlur={() => {
+        setExpanded(false);
+      }}
     />
   );
 };
